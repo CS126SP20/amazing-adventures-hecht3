d9522f90556043e1c78585e428b945acee42d893
connell <hecht3@illinois.edu>
2020-02-08 15:17:21 -0600

    Some evaluateInput tests and basic invalid input evaluation code

--- src/main/java/student/adventure/Adventure.java
+++ src/main/java/student/adventure/Adventure.java
@@ -3,18 +3,42 @@ package student.adventure;
 import java.util.Scanner;
 
 public class Adventure {
-    public void displayMessage(String message) {
 
-    }
-    public void readInput() { /////Not sure what parameters should go in this method declaration
+
+    public static void readInput() { /////Not sure what parameters should go in this method declaration
         Scanner scanner = new Scanner(System.in);
-        String directionToMove = scanner.nextLine();
+        String input = scanner.nextLine();
+        evaluateInput(input);
 
     }
-    public void evaluateInput(String input) {
+    public static void evaluateInput(String input) {
         //Also is in charge of changing the state of the game (i.e. what room the user is "in")
+        String inputAsLower = input.toLowerCase().trim();
+        String direction = null;
+        String[] possibleDirections = {"North", "South", "East", "West"};
+        String currentRoom;
+
+        for (int i = 0; i < possibleDirections.length; i++) {
+            if (possibleDirections[i].toLowerCase().equals(inputAsLower)) {
+                direction = possibleDirections[i];
+            }
+        }
+
+        if (direction != null) {
+            currentRoom = changeRoom(direction);
+        } else {
+            System.out.println("I don't understand '" + input + "'");
+            //readInput();
+        }
+
+        String toPrint = "";
+        //isEndOfGame(currentRoom);
     }
-    public boolean isEndOfGame(String currentRoom) {
+    public static String changeRoom(String direction) {
+        String currentRoom = "";
+        return currentRoom;
+    }
+    public static boolean isEndOfGame(String currentRoom) {
         return false;
     }
 }
--- src/test/java/student/adventure/AdventureTest.java
+++ src/test/java/student/adventure/AdventureTest.java
@@ -1,24 +1,75 @@
 package student.adventure;
 
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.CoreMatchers;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 
 public class AdventureTest {
     private ObjectMapper mapper;
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
     @Before
     public void setUp() {
         mapper = new ObjectMapper();
     }
 
+    //The following are tests using the standard siebel.json file
+
+    //The helper function tests
+    //readInput does not need to be tested because it is trivial
+
     @Test
-    public void sanityCheck() {
-        // TODO: Remove this unnecessary test case.
-        assertThat("CS 126: Software Design Studio", CoreMatchers.containsString("Software"));
+    public void evaluateInputInvalidString() {
+        Adventure.evaluateInput("Hello");
+        assertEquals("I don't understand 'Hello'\n", systemOutRule.getLog());
+    }
+    @Test
+    public void evaluateInputInvalidStringNumber() {
+        Adventure.evaluateInput("5");
+        assertEquals("I don't understand '5'\n", systemOutRule.getLog());
+    }
+    @Test
+    public void evaluateInputInvalidStringCapitalization() {
+        Adventure.evaluateInput("hELlo");
+        assertEquals("I don't understand 'hELlo'\n", systemOutRule.getLog());
+    }
+    @Test
+    public void initialGoEastExtraLength() {
+        Adventure.evaluateInput("go East and then go NORth");
+        assertEquals("I don't understand 'go East and then go NORth'\n",
+                systemOutRule.getLog());
+    }
+    @Test
+    public void initialGoEast() {
+        Adventure.evaluateInput("go east");
+        assertEquals("You are in the west entry of Siebel Center. " +
+                "You can see the elevator, the ACM office, and hallways to the north and east.\n" +
+                "From here, you can go: West, Northeast, North, or East\n", systemOutRule.getLog());
+    }
+    @Test
+    public void initialGoEastCapitalization() {
+        Adventure.evaluateInput("gO EaSt");
+        assertEquals("You are in the west entry of Siebel Center. " +
+                "You can see the elevator, the ACM office, and hallways to the north and east.\n" +
+                "From here, you can go: West, Northeast, North, or East\n", systemOutRule.getLog());
+    }
+    @Test
+    public void initialGoEastExtraLengthSpaces() {
+        Adventure.evaluateInput("   go East     ");
+        assertEquals("You are in the west entry of Siebel Center. " +
+                "You can see the elevator, the ACM office, and hallways to the north and east.\n" +
+                "From here, you can go: West, Northeast, North, or East\n", systemOutRule.getLog());
     }
 }
